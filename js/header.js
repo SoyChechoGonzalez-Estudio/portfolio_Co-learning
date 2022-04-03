1c9f6103a0cf4c89a42e6743d6a168da2f3082c1
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2022-04-03 18:40:14 -0500

    Se termina la preparación del Co-learning

---
+++ js/header.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.getElementById('nav');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
